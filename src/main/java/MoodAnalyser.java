e70bd680848cc66a2838492b9fcbdd3bcf8319ea
poojagaikwad1358 <poojagaikwad1358@gmail.com>
2020-08-13 09:29:41 +0530

    Added: Method, consructor & test cases for happy and sad mood.

--- src/main/java/MoodAnalyser.java
+++ src/main/java/MoodAnalyser.java
@@ -1,4 +1,25 @@
 
 public class MoodAnalyser {
+    String message;
+
+    //Default Constructor
+    public MoodAnalyser() {
+    }
+
+    //Parameterised Constructor
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    //Method for checking happy or sad mood
+    public String analyseMood(String message) {
+        if (message.contains("I am in sad mood.")) {
+            return "SAD";
+        } else if (message.contains("I am in any mood.")) {
+            return "HAPPY";
+        }
+        else
+            return "null";
+    }
 
 }
--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -1,4 +1,20 @@
+import org.junit.Assert;
+import org.junit.Test;
 
 public class MoodAnalyserTest {
+    MoodAnalyser moodAnalyser = new MoodAnalyser();
 
+    //Test case for happy mood
+    @Test
+    public void happyMood_returnHappy_ifMoodEqualsOtherThanSad() {
+        String mood = moodAnalyser.analyseMood("I am in any mood.");
+        Assert.assertEquals("HAPPY",mood);
+    }
+
+    //Test case for sad mood
+    @Test
+    public void sadMood_returnSad_ifMoodEqualsSad() {
+        String mood = moodAnalyser.analyseMood("I am in sad mood.");
+        Assert.assertEquals("SAD",mood);
+    }
 }
